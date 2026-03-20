5254d38bc0a2ec5055b4b3dfdf60098e8ef72d0b
Bao Pham <louis.phammm@gmail.com>
2026-03-21 00:15:18 +0700

    Fix werkzeug missing session.sid causing 303 Redirect in check_session

--- pos_theme_swift/models/ir_http.py
+++ pos_theme_swift/models/ir_http.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import logging
 import odoo
 from odoo import models
 from odoo.http import request
+
+_logger = logging.getLogger(__name__)
 
 class IrHttp(models.AbstractModel):
     _inherit = 'ir.http'
@@ -13,13 +16,15 @@ class IrHttp(models.AbstractModel):
         auth_header = request.httprequest.headers.get('Authorization', '')
         if auth_header.lower().startswith('bearer '):
             token = auth_header[7:].strip()
+            _logger.info("Swift API Auth Header found. Token: %s", token)
             if token and token != request.session.sid:
-                # Load the session from the token provided in the header
-                # This will find the session file on the server and set the user/db
                 new_session = odoo.http.root.session_store.get(token)
+                _logger.info("Swift API Extracted Session: %s, uid: %s, db: %s", bool(new_session), new_session.uid if new_session else 'None', new_session.db if new_session else 'None')
                 if new_session:
+                    new_session.sid = token  # MUST BE SET: Werkzeug does not set .sid on restored sessions!
                     request.session = new_session
                     if new_session.uid:
                         request.update_env(user=new_session.uid, context=new_session.context)
+                        _logger.info("Swift API Environment Updated: Env UID is now %s", request.env.uid)
         
         return super(IrHttp, cls)._authenticate(endpoint)
